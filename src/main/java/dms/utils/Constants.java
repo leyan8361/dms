b9643de28f1cee4da504927fdc2c685ec498675f
miweijia9472 <434606959@qq.com>
2018-08-14 21:06:59 +0800

    1

--- src/main/java/dms/utils/Constants.java
+++ src/main/java/dms/utils/Constants.java
@@ -4,4 +4,10 @@ public class Constants {
 
 	// token的密钥
 	public static final String tokenSecret = "dms_token";
+
+	// 接口返回的状态码 (成功、接口出错、token信息出错、token过期)
+	public static final String successStatus = "0";
+	public static final String apiErrorStatus = "1";
+	public static final String tokenErrorStatus = "2";
+	public static final String tokenOverTimeStatus = "3";
 }
