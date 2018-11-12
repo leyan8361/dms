8490a0cf0d7b669b9c78c6d084268bad48badd51
ACER <ACER@DESKTOP-CI1J3K7>
2018-11-12 10:46:36 +0800

    3

--- src/main/java/dms/controller/ErrorController.java
+++ src/main/java/dms/controller/ErrorController.java
@@ -1,12 +1,16 @@
 package dms.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
@@ -15,7 +19,7 @@ import com.alibaba.fastjson.JSON;
 @RestController
 @RequestMapping("error")
 public class ErrorController {
-
+	
 	/**
 	 * 返回错误接口
 	 * 
@@ -29,5 +33,20 @@ public class ErrorController {
 		resMap.put("status", req.getParameter("status"));
 		resMap.put("info", req.getParameter("info"));
 		return JSON.toJSONString(resMap);
+	}
+
+	/**
+	 * 返回项目版本号
+	 * @return
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "getVersion", method = RequestMethod.GET, produces = "text/text;charset=UTF-8")
+	public String getVersion() throws IOException {
+
+		InputStream in = this.getClass().getResourceAsStream("/config.properties");
+		Properties properties = new Properties();
+		properties.load(in);
+		String s = properties.getProperty("version");
+		return s;
 	}
 }
